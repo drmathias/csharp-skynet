b5c0aedfcde6e690a4933b4d7aa871f73fc74129
Adam Shirt <adam.shirt@developmomentum.com>
2020-07-29 21:22:53 +0100

    Correct Bitfield data type

--- src/Sia.Skynet/UploadResponse.cs
+++ src/Sia.Skynet/UploadResponse.cs
@@ -11,13 +11,13 @@
         public string Skylink { get; set; }
 
         /// <summary>
-        /// The merkleroot
+        /// The hash that is encoded into the Skylink
         /// </summary>
         public string Merkleroot { get; set; }
 
         /// <summary>
-        /// The bitfield
+        /// The bitfield contains a version, an offset and a length in a heavily compressed and optimized format
         /// </summary>
-        public byte Bitfield { get; set; }
+        public ushort Bitfield { get; set; }
     }
 }
